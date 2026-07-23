fbb4e7d8e10a10f36db58b978f02ae6df7f8f92f
jung <jungseiyoon1@gmail.com>
2026-07-23 17:41:45 +0900

    게임코어

--- Assets/Scripts/Game Core/GameManager.cs
+++ Assets/Scripts/Game Core/GameManager.cs
@@ -4,6 +4,42 @@ public class GameManager : MonoBehaviour
 {
     private Order currentOder;
     private MixResult currentMixResult;
+    public GameTimer createGameTimer()
+    {
+        return new GameTimer();
+    }
+   public void UseGaneTimer(GameTimer gameTimer)
+    {
+
+    }
+    public ScoreSystem createScoreSystem()
+    {
+        return new ScoreSystem();
+    }
+    public void UseScoreSystem(ScoreSystem scoreSystem)
+    {
+         
+    }
+    
+    public UIMatager createUIManager()
+    {
+        return new UIMatager();
+    }
+
+    public void UseUImanager(UIMatager uIMatager)
+    {
+
+    }
+    public SoundManager createSoundManager()
+    {
+        return new SoundManager();
+    }
+
+    public void UseSoundManager(SoundManager soundManager)
+    {
+
+    }
+
     public void StartGame()
     {
 
