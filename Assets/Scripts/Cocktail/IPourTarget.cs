430681759c5bb42db3f87fdc6ee4b5c717ca517a
qoralstmd6825 <qoralstmd6825@gmail.com>
2026-07-30 19:44:13 +0900

    feat: Cup,MixingCup, IPourTarget 제작 및 구현

--- Assets/Scripts/Cocktail/IPourTarget.cs
+++ Assets/Scripts/Cocktail/IPourTarget.cs
@@ -3,6 +3,6 @@ using UnityEngine.UIElements;
 
 public interface IPourTarget
 {
-    public bool IsInsideMouth(Vector3 point);
-    public bool SetFill(float ratio, Color color);
+    public bool IsInsideMouth(Vector2 point);
+    public void SetFill(float ratio, Color color);
 }
--- Assets/Scripts/Cocktail/MixingCup.cs
+++ Assets/Scripts/Cocktail/MixingCup.cs
@@ -1,22 +1,84 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class MixingCup : MonoBehaviour
+public class MixingCup : MonoBehaviour, IPourTarget
 {
-    private DragInput drag;
+    // 이게 액체가 들어갔는 판정을 할 렌더러
+    [SerializeField] 
     private SpriteRenderer mouthRenderer;
+    // 액체가 담겼을때 채워질 렌더러
+    [SerializeField] 
     private SpriteRenderer fillRenderer;
-    private CupMode mode;
-    private Vector3 homePos;
-    private Action onSubmit;
-    private Action<Vector2> onThrowRelease;
 
-    public void SetMode(CupMode mode)
+    private Coroutine pourCoroutine;
+    
+    private Vector2 originPos;
+    
+    private void Start()
     {
-
+        originPos = transform.position;
     }
-    public void SetSubmitHandler()
+    
+    // 컵에 액체를 따르는 코루틴을 실행하는 메소드
+    // ratio 는 액체가 차오르는 목표 비율
+    public void PourInto(Cup cup, float ratio, float duration, Color color ,Action onDone)
     {
+        if(pourCoroutine != null) StopCoroutine(pourCoroutine);
+        pourCoroutine = StartCoroutine(PourCoroutine(cup, ratio, duration, color, onDone));
+    }
+    
+    // duration 에 비례해서 액체가 줄어들고 컵의 액체는 늘어나게 연출
+    private IEnumerator PourCoroutine(Cup cup, float ratio, float duration , Color color ,Action onDone)
+    {
+        float timer = 0;
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            float percent = timer / duration;
+            cup.SetFill(ratio * percent, color);
+            SetFill(ratio * (1 - percent), color);
+            yield return null;
+        }
+        cup.SetFill(ratio, color);
+        SetFill(0f, color);
+        pourCoroutine = null;
+        onDone?.Invoke();
+    }
+    
+    // 원래 자리로 돌아가게 하기
+    // 나중에 컵이 따르는 연출 할때 쓰는걸로
+    public void ResetToHome()
+    {
+        transform.position = originPos;
+        transform.rotation = Quaternion.identity;
+    }
 
+    // 중간에 멈추게 될때 사용
+    public void AbortPour()
+    {
+        if(pourCoroutine != null)
+        {
+            StopCoroutine(pourCoroutine);
+            pourCoroutine = null;
+        }
+        SetFill(0f, Color.white);
+    }
+
+    
+    // 도구 용기에 재료가 들어갔는지 판정
+    public bool IsInsideMouth(Vector2 point)
+    {
+        return mouthRenderer.bounds.Contains(point);
+    }
+    
+    // 특정 색으로 채우기
+    public void SetFill(float ratio, Color color)
+    {
+        fillRenderer.color = color;
+        Vector3 localScale = fillRenderer.transform.localScale;
+        localScale.y = ratio;
+        fillRenderer.transform.localScale = localScale;
     }
 }
