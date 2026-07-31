03c26d0ab866007675808bc96778e538ba8867d9
jung <jungseiyoon1@gmail.com>
2026-07-31 12:13:58 +0900

    Feat : Timer 기능 구현
    포크 버그로 인한 재푸쉬

--- Assets/Scripts/Game Core/GameTimer.cs
+++ Assets/Scripts/Game Core/GameTimer.cs
@@ -1,26 +1,57 @@
+using System;
 using UnityEngine;
 
 public class GameTimer : MonoBehaviour
 {
-    private float remainTime;
+    private float remaining;
+    private bool isRunning;
+    private float maxTime;
 
+    public event Action OnTimeOver;
 
-    public void Start()
+    private void Update()
     {
-        
+
+        if (!isRunning)
+        {
+            return;
+        }
+
+        remaining -= Time.deltaTime;
+
+        if (remaining <= 0)
+        {
+            remaining = 0;
+            isRunning = false;
+            OnTimeOver?.Invoke();
+        }
     }
+
+    public void Prepare(float second)
+    {
+        isRunning = false;
+        maxTime = second;
+        remaining = second;
+    }
+
+    public void Begin()
+    {
+        isRunning = true;
+    }
+
+    public void AddBonus(float second)
+    {
+
+        remaining += second;
+        if (remaining > maxTime)
+        {
+            remaining = maxTime;
+        }
+    }
+
     public void Stop()
     {
 
-    }
-
-    public void Reset()
-    {
-        
-    }
-
-    public void AddBonusTime(float seconds)
-    {
-        
+        isRunning = false;
     }
 }
