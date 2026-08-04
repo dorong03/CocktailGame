6dd1c45766c53658b0ef5eeaff9bef4162fd2183
jung <jungseiyoon1@gmail.com>
2026-08-04 16:37:23 +0900

    feat : ServingController 구현

--- Assets/Scripts/Serving/ServingController.cs
+++ Assets/Scripts/Serving/ServingController.cs
@@ -3,21 +3,36 @@ using UnityEngine;
 
 public class ServingController : MonoBehaviour
 {
-    public Action<Grade> OnServingCompleted;
+    // 던지는 컵
+    private Cup cup;
 
-    private MixResult result;
-    private int seatIndex;
+    // npc 자리 앞에 있는 시트
+    private Seat targetSeat;
 
-    public void StartServing()
+    // 판정이 완료 됐을때 실행할 메소드
+    private Action<Grade> onComplete;
+
+    // 외부에서 서빙을 시작하는 메소드
+    public void StartServing(Seat seat, Action<Grade> onComplete)
     {
-
+        targetSeat = seat;
+        this.onComplete = onComplete;
+        cup.SetMode(CupMode.Throwable);
+        cup.SetThrowHandler(CupLandHandler);
     }
-    public void ThrowCocktail()
-    {
 
-    }
-    public Grade Judge()
+    // 중지하는 메소드 pause
+    public void Abort()
     {
-        return 0;
+        cup.SetMode(CupMode.Locked);
+        targetSeat = null;
+        onComplete = null;
+    }
+
+    //판정
+    private void CupLandHandler(Vector2 cupPosition)
+    {
+        Grade grade = targetSeat.EvaluateLanding(cupPosition);
+        onComplete(grade);
     }
 }
