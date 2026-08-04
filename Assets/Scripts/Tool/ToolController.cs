0c82a2f05b06ec8adf4e41f70d18b9edb84972ab
geunhojo483-design <geunhojo483@gmail.com>
2026-08-04 16:39:28 +0900

    feat : ToolController 기능 구현 추가

--- Assets/Scripts/Tool/ToolController.cs
+++ Assets/Scripts/Tool/ToolController.cs
@@ -1,15 +1,43 @@
+using System;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class ToolController : MonoBehaviour
 {
+
+    [SerializeField]
+    private ShakerTool shaker;
+    [SerializeField]
+    private BarSpoonTool barSpoon;
+
     private ToolBase currentTool;
 
-    public void StartTool(string toolId)
+    public void Arm(string tool, Func<bool> canStart,Action onBlocked, Action onStarted, Action onComplete)
     {
-
+        currentTool = GetToolById(tool);
+        currentTool.ActiveTool(canStart, onBlocked, onStarted, onComplete);
     }
-    public void FinishTool()
+
+    private ToolBase GetToolById(string id)
     {
-        
+        switch(id)
+        {
+            case "TOOL_000":
+                return shaker;
+            case "TOOL_001":
+                return barSpoon;
+            default:
+                Debug.LogError($"존재하지 않는 도구 아이디 -> {id}");
+                return null;
+        }
+    }
+
+    public void Abort()
+    {
+        currentTool.Abort();
+        currentTool = null;
     }
 }
