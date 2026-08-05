4fb0a7f4a2a88ef78492aed0d45fb998a46a40f8
jung <jungseiyoon1@gmail.com>
2026-08-05 17:15:02 +0900

    feat : NpcController기능구현

--- Assets/Scripts/NPC/NpcController.cs
+++ Assets/Scripts/NPC/NpcController.cs
@@ -1,24 +1,86 @@
+using System;
+using System.Collections;
+using Unity.Android.Gradle;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class NpcController : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
     private NpcData currentNpc;
-    private Order currentOrder;
+    private DataRepository data;
 
-    public void SpawnNpc()
-    {
+    [SerializeField]
+    private float moveDuration = 1f;
+    [SerializeField]
+    private float moveDistance = 6f;
+    private float moveSpeed;
 
-    }
-    public Order CreateOrder()
-    {
-        return null;
-    }
-    public void Dismiss()
-    {
+    private bool isMoving;
 
+    public Seat seat;
+
+    private void Awake()
+    { 
+        data = DataRepository.Instance;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        moveSpeed = moveDistance / moveDuration;
+        isMoving = false;
+        Clear();
     }
-    public Order GetCurrentOrder()
+
+    // npc 가 특정 자리에 나오는 연출
+    public void SpawnNpc(NpcData npc, Seat seat, Action onArrived)
     {
-        return null;
+        //gameObject.SetActive(true);
+        transform.position = seat.transform.position;
+        StartCoroutine(PositionUp());
+        onArrived?.Invoke();
+    }
+
+    //떠날때
+    public void Depart(Action onGone)
+    {
+        transform.position = seat.transform.position + Vector3.up * moveDistance;
+        StartCoroutine(PositionDown());
+        onGone?.Invoke();
+    }
+
+    private IEnumerator PositionUp()
+    {
+        isMoving = true;
+        float timer = 0f;
+
+        while (timer < moveDuration)
+        {
+            transform.position += Vector3.up * (Time.deltaTime * moveSpeed);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = seat.transform.position + moveDistance * Vector3.up;
+        isMoving = false;
+    }
+
+    private IEnumerator PositionDown()
+    {
+        isMoving = true;
+        float timer = 0f;
+
+        while (timer < moveDuration)
+        {
+            transform.position -= Vector3.up * (Time.deltaTime * moveSpeed);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = seat.transform.position;
+        isMoving = false;
+    }
+
+    // 초기화
+    public void Clear()
+    {
+        //gameObject.SetActive(false);
     }
 }
