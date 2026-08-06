83f994c3cc7b8d6f9b4dc22b14114adb0c93f616
geunhojo483-design <geunhojo483@gmail.com>
2026-08-06 18:20:51 +0900

    feat : ScoreCalculator 기능 구현

--- Assets/Scripts/Score/ScoreCalculator.cs
+++ Assets/Scripts/Score/ScoreCalculator.cs
@@ -1,25 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 static class ScoreCalculator
 {
-    static RecipeData recipe;
-    static MixResult result;
+    private static float[] bonusTime = { 5f, 3f, 2f, 1f, 0f };
+    
 
-    static int baseScore;
-    static float accuracy;
-
-    static bool isMiss;
-
-    public static float CalculateAccuracy()
+    // 각 재료별 정확도 
+    private static float GetIngredientAccuracy(float targetAmount, float actualAmount)
     {
-        return 0;
+        float ingredientAccuracy = Mathf.Max(0, 1 - (Mathf.Abs(actualAmount - targetAmount) / targetAmount));
+
+        return ingredientAccuracy;
     }
-    public static int CalculateCocktailScore()
+
+    // 재료 정확도의 평균값
+    public static float GetMixAccuracy(RecipeData recipe, MixResult result)
     {
-        return 0;
+        Dictionary<string, float> targetMixes = new Dictionary<string, float>();
+
+        int mixCount = result.Ingredients.Count;
+        float totalAccuracy = 0f;
+
+        foreach (var targetMix in recipe.Ingredients)
+        {
+            targetMixes.Add(targetMix.IngredientId, targetMix.Amount);
+        }
+
+        foreach(var actualMix in result.Ingredients)
+        {
+            // targetAmount 에 레시피 용량을 저장함
+            targetMixes.TryGetValue(actualMix.IngredientId, out float targetAmount);
+            // actualAmount 에 내가 담은 용량을 저장함
+            float actualAmount = actualMix.Amount;
+            totalAccuracy += GetIngredientAccuracy(targetAmount, actualAmount);
+        }
+        float finalAccuracy = totalAccuracy / mixCount;
+        return finalAccuracy;
     }
-    public static int CalculateFinalScore()
+
+    // 최종 점수
+    public static int GetFinalScore(int baseScore, float accuracy)
     {
-        return 0;
+        // 레시피의 베이스 스코어와 재료 정확도의 점수, 시트 점수를 총함하여 보내기
+        int convertAccuracy = Convert.ToInt32(accuracy);
+        return baseScore * convertAccuracy;
+    }
+
+    // Grade 를 받아서 시간 반환해주기
+    public static float GetBonusSeconds(Grade grade)
+    {
+        return bonusTime[(int)grade];
     }
 }
